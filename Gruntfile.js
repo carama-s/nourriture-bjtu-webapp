5034b291f88b4c8b60ccce26c2447356a73ea10c
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-17 17:07:09 +0800

    add less and watch task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,6 @@ module.exports = function(grunt) {
         },
       },
       uglify: {
-        // Grunt will search for "**/*.js" under "lib/" when the "uglify" task
-        // runs and build the appropriate src-dest file mappings then, so you
-        // don't need to update the Gruntfile when files are added or removed.
         files: [
           {
             expand: true,     // Enable dynamic expansion.
@@ -28,14 +25,39 @@ module.exports = function(grunt) {
           }
         ]
       }
+    },
+    less: {
+      options: {
+        compress: true,
+        optimization: 2
+      },
+      files: {
+        expand: true,     // Enable dynamic expansion.
+        cwd: 'www/styles/less/',      // Src matches are relative to this path.
+        src: ['**/*.less'], // Actual pattern(s) to match.
+        dest: 'www/styles/lcss/',   // Destination path prefix.
+        ext: '.css',   // Dest filepaths will have this extension.
+        extDot: 'first'   // Extensions in filenames begin after the first dot
+      }
+    },
+    watch: {
+      styles: {
+        files: ['www/styles/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['watch']);
 
-  grunt.registerTask('deploy', ['uglify']);    
+  grunt.registerTask('deploy', ['uglify', 'less']);
 };
