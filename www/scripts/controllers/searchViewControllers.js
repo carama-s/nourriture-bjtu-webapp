866fd6773f783946dfb2068aedf4b7929950b573
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-01-10 22:15:51 +0800

    search almost finished

--- www/scripts/controllers/searchViewControllers.js
+++ www/scripts/controllers/searchViewControllers.js
@@ -20,10 +20,63 @@ searchViewControllers.controller('SearchViewResult', ['$scope', "$location", 'ap
     var qs = $location.search();
     if (!qs.q) return $location.url("/");
 
-    apiFactory.common.search({
-      params: {search: qs.q}
-    }).then(function(res) {
-      $scope.results = res.data;
-    });
+    if (qs.what == undefined) $scope.selectedCategory = "all";
+    else if (_.contains(["all", "recipe", "ingredient"], qs.what)) $scope.selectedCategory = qs.what;
+    else return $location.url("/");
+
+    $scope.modeDisplay = "module";
+    $scope.nbElements = 0;
+    $scope.nbPage = 0;
+    $scope.currentPage = 0;
+    $scope.perPage = 9;
+
+    function changeUrl(q, what) {
+      $location.url("/search?q=" + encodeURI(q) + "&what=" + what);
+    }
+
+    $scope.changeCategory = function(v) {
+      changeUrl(qs.q, v);
+    };
+
+    $scope.switchView = function(v) {
+      $scope.modeDisplay = v;
+    };
+
+    $scope.canGoPreviousPage = function() {
+      return $scope.currentPage > 0;
+    }
+
+    $scope.goPreviousPage = function() {
+      if ($scope.canGoPreviousPage()) {
+        $scope.currentPage -= 1;
+        doSearch();
+      }
+    };
+
+    $scope.canGoNextPage = function() {
+      return ($scope.currentPage + 1) < $scope.nbPage;
+    }
+
+    $scope.goNextPage = function() {
+      if ($scope.canGoNextPage()) {
+        $scope.currentPage += 1;
+        doSearch();
+      }
+    };
+
+    function doSearch() {
+      var params = {search: qs.q};
+      if (_.contains(["recipe", "ingredient"], qs.what)) params.what = qs.what;
+      params.limit = $scope.perPage;
+      params.skip = $scope.currentPage * $scope.perPage;
+
+      apiFactory.common.search({params: params}).then(function(res) {
+        $scope.nbElements = res.data.total;
+        $scope.nbPage = Math.ceil(res.data.total / $scope.perPage);
+        $scope.results = res.data.results;
+      });
+    }
+
+    doSearch();
   }
 ]);
